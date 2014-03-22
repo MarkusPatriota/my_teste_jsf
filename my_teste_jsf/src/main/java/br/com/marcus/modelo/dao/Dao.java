c028c29a693817d674e16b02c7026bc8928a2e1b
Markus <Markus@ASUS-PC>
2014-03-21 21:54:59 -0300

    Implementação do Dao

--- my_teste_jsf/src/main/java/br/com/marcus/modelo/dao/Dao.java
+++ my_teste_jsf/src/main/java/br/com/marcus/modelo/dao/Dao.java
@@ -2,7 +2,6 @@ package br.com.marcus.modelo.dao;
 
 import java.io.Serializable;
 import java.util.List;
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.DetachedCriteria;
 
